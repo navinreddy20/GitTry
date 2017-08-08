caaf4cbaf63fc60a172e7f48528d6b48abd48f98
navinreddy <navinreddy@192.168.0.103>
2017-08-08 22:00:32 +0530

    Added calc class and runner updated

---
+++ GitTry/src/com/telusko/Calculator.java
@@ -0,0 +1,9 @@
+package com.telusko;
+
+public class Calculator 
+{
+	public void add(int i,int j)
+	{
+		System.out.println(i+j);
+	}
+}
--- GitTry/src/com/telusko/Runner.java
+++ GitTry/src/com/telusko/Runner.java
@@ -3,7 +3,8 @@ package com.telusko;
 public class Runner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+			System.out.println("Hello World");
 
 	}
 
